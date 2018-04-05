a6d0f0a217b9052ab200658dd624f318df26ea72
seasmail <kkeennny@gmail.com>
2018-04-05 23:00:13 +0300

    Add files via upload

---
+++ ClientRepository.java
@@ -0,0 +1,6 @@
+package com.devcolibri.dataexam.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Class, Long> {
+}
